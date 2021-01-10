35d66fea7ab047721612d5f947b6ff4cbe15bcc3
Cornus <cornus@MacBook-Pro-Cornus.local>
2021-01-10 08:15:01 +0100

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    """ dłuższy opis komentarza """
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # TODO: do wyświetlania na dole w zakładce todo
+    print_hi('PyCharm')
+    import os
+    os.path.join(os.getcwd(), 'podfolder1')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
